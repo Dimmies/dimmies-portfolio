a5b06c7a5ab01b06ee45c12c1feb32ca089378bd
Dimmies <dimmiestv@gmail.com>
2024-12-05 15:23:00 -0500

    add arp inventory

--- fivem/fivem.js
+++ fivem/fivem.js
@@ -1,5 +1,8 @@
+let arpInvInfoShown = false
+
 $( document ).ready(function() {
     $("#fullview").fadeOut(0)
+    $(".arp-info").fadeOut(0)
     $(".gallery-cell > img").on("click", function(ele) {
         $("#fullview-img").attr("src", $(ele.target).attr("src"))
         $("#fullview").fadeIn(300)
@@ -7,6 +10,14 @@ $( document ).ready(function() {
 
     $("#fullview").on("click", function(ele) {
         $("#fullview").fadeOut(300)
+    })
+
+    $(".learn-more").on("click", function(ele) {
+        $(".arp-info").fadeIn(300)
+    })
+
+    $(".close-info").on("click", function(ele) {
+        $(".arp-info").fadeOut(300)
     })
 });
 
